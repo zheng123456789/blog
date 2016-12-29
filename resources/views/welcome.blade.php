fcb69e3f135f166fb22065817678b436f4fc7e03
zheng <1065628795@qq.com>
2016-12-29 23:43:38 +0800

    two

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,91 +1,169 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    <a href="{{ url('/login') }}">登录</a>
-                    <a href="{{ url('/register') }}">Register</a>
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    XXOO
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">jkl;</a>
-                    <a href="https://laracasts.com">adsf</a>
-                    <a href="https://laravel-news.com">新闻</a>
-                    <a href="https://forge.laravel.com">ooxx</a>
-                    <a href="https://github.com/laravel/laravel">xxoo</a>
-                </div>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>郑勇的个人博客</title>
+<meta name="keywords" content="free css template, orange bar, XHTML, CSS" />
+<meta name="description" content="Orange Bar is a free CSS template from cssMoban.com" />
+<link href="{{ asset('css/templatemo_style.css') }}" rel="stylesheet" type="text/css" />
+<script language="javascript" src="{{ asset('js/templatemo_image_fader.js') }}" type="text/javascript"></script>
+<script language="javascript" type="text/javascript">
+function clearText(field)
+{
+    if (field.defaultValue == field.value) field.value = '';
+    else if (field.value == '') field.value = field.defaultValue;
+}
+</script>
+</head>
+<body>
+    <div class="templatemo_container">
+        <div id="templatemo_header">
+            <div id="templatemo_logo_area">
+                <div id="templatemo_logo">
+                    <a href="http://www.cssmoban.com" target="_blank">H1标题</a></div>
+              <div id="templatemo_slogan">
+                    小标题
+              </div>
+                <div class="cleaner"></div>
             </div>
+            
+            <div id="templatemo_search">
+                <form action="#" method="post">
+                    <input type="text" value="请输入搜索关键字" name="q" class="field"  title="email" onfocus="clearText(this)" onblur="clearText(this)" />
+                    <input type="submit" name="search" value = "搜索" alt="Search" class="button" title="Subscribe" />
+            </form>
+            </div>
+        
+            <div id="templatemo_menu">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li class="current"><a href="#">Gallery/画廊</a></li>
+                    <li><a href="#">Archives/档案</a></li>
+                    <!-- <li><a href="#">Wallpapers/壁纸</a></li>                         -->
+                    <li><a href="#">Downloads/下载</a></li>
+                    <li><a href="http://www.cssmoban.com" target="_parent">Templates/模板</a></li>   
+                    <li><a href="#">Contact/联系</a></li>      
+                </ul>
+            </div>
+            
+        </div><!-- end of header -->
+        
+        <div id="templatemo_image_fader">
+             <script type="text/javascript">
+//new fadeshow(IMAGES_ARRAY_NAME, slideshow_width, slideshow_height, borderwidth, delay, pause (0=no, 1=yes), optionalRandomOrder)
+new fadeshow(fadeimages, 960, 240, 1, 2000, 1, "R")
+ 
+            </script>
         </div>
-    </body>
+                
+        <div id="templatemo_content_area">
+            <div id="templatemo_left">
+                <div class="templatemo_post">
+                    <div class="templatemo_post_title">
+                        <a href="#">Free Website Layout</a></div>
+                    <div class="templatemo_post_text">
+                        <a href="#"><img src="{{ asset('images/templatemo_thumb_1.jpg') }}" alt="Flower" width="150" height="150" /></a>
+                        <p>This is a free CSS template provided by <a href="http://www.cssmoban.com" target="_parent">缃戦〉妯℃澘</a> website. You may download and apply this  layout for  your personal or commercial websites. Credit goes to <a href="#" target="_blank">PhotoVaco.com</a> for photos used in this template. </p>
+                        <p>Vivamus ultricies bibendum viverra. Sagittis vel puru and aien ai dofneonsets srci. In vulputate, felis vitae ultrices dapibus, leo nulla ornare erat, eu tincidunt magna ipm sit amet massdtess vivamus sed condimentum ipsum. Quisque ut tortor in elit luctus.</p>
+                  </div>
+                    
+      <div class="templatemo_post_bottom">
+                        <a href="#">24 comments</a></div>
+                    
+              </div>
+                
+                <div class="templatemo_post">
+                    <div class="templatemo_post_title">
+                        <a href="#">Web Design Studio</a></div>
+                    <div class="templatemo_post_text">
+                        <a href="#"><img src="{{ asset('images/templatemo_thumb_2.jpg') }}" alt="Yellow Rose" width="150" height="150" /></a>
+                        <p>Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus. Praesent elementum hendrerit tortor.</p>
+                        <p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu pulvinar risus, vitae facilisis libero dolor a purus. Sed vel lacus. Mauris nibh felis, adipiscing varius, adipiscing in.</p>                   
+                    </div>
+                    
+      <div class="templatemo_post_bottom">
+                        <a href="#">56 comments</a></div>
+                    
+              </div>
+            </div><!-- End Of left-->
+            
+            <div id="templatemo_right">
+                <div class="templatemo_section">
+                    <div class="templatemo_section_title">
+                        Categories
+                    </div>
+                    <div class="templatemo_section_bottom">
+                        <ul class="templatemo_list">
+                            <li><a href="#">Amet massdtess vivamus sed condimentum ipsum</a></li>
+                            <li><a href="#">Donec nec justo eget felis facilisis fermentum</a></li>
+                            <li><a href="#">Fusce lobortis lorem at ipsum sem</a></li>
+                            <li><a href="#">Nulla vitae mau ris non felis mollis fau cibus</a></li>
+                            <li><a href="#">Cras ornare tristique elit conval lis</a></li>
+                        </ul>
+                    </div>
+                </div>
+                    
+                <div class="templatemo_section">
+                    <div class="templatemo_section_title">
+                        Popular Posts
+                    </div>
+                    <div class="templatemo_section_bottom">
+                <ul class="templatemo_list">
+                            <li><a href="#">Morbi in sem quis dui plac erat orn are. Pell len tesque odio nisi, euis mod in.</a></li>
+                            <li><a href="#">Pell len tesque fer men tum dolor. Ali quam quam lectus, facilisis auc tor ultri.</a></li>
+                            <li><a href="#">Nulla sed leo. Class aptent taciti socio squ ad litora tor quent per conubia.</a></li>
+                            <li><a href="#">Fusce lacinia arcu et nulla. Nulla vitae mau ris non felis mollis fau cibus.</a></li>
+                            <li><a href="#">Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci.</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div><!-- End Of right-->
+            
+            <div class="cleaner"></div>
+        
+        </div><!-- End Of Content area -->
+    </div><!-- End Of Container -->
+    
+    <div id="templatemo_bottom_area">
+        <div class="templatemo_container">
+            <div class="templatemo_section_1">
+                <h1>Resource Links</h1>
+                <ul class="templatemo_list templatemo_gray_list">
+                    <li><a href="http://www.cssmoban.com" target="_parent">Free Web Templates</a></li>
+                    <li><a href="#" target="_blank">Flash Templates</a></li>
+                    <li><a href="#" target="_blank">Web Design Tips</a></li>
+                    <li><a href="#" target="_blank">Flash Web Gallery</a></li>
+                    <li><a href="#">WordPress Themes</a></li>
+                </ul>
+            </div>
+            
+            <div class="templatemo_section_1">
+                <h1>Recent Posts</h1>  
+                <ul class="templatemo_list templatemo_gray_list">
+                    <li><a href="#">Phasellus ultrices nulla</a></li> 
+                    <li><a href="#">Quis nibh quisque ale</a></li> 
+                    <li><a href="#">Donec con sect tetuer</a></li>
+                    <li><a href="#">Lig ula vul pu tate sem</a></li>
+                    <li><a href="#">Tist ique cursus nam</a></li>
+                </ul>
+                               
+            </div>
+            
+            <div class="templatemo_section_2">
+                <h1>W3C Validations</h1>
+                <p>Ut convallis, sem sit amet interdum consectetuer, odio augue aliquam leo, nec dapibus tortor nibh sed augue. Integer eu magna sit amet metus fermentum posuere. Morbi sit amet nulla sed dolor elementum.</p>
+
+          <a href="http://validator.w3.org/check?uri=referer"><img style="border:0;width:88px;height:31px" src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Transitional" width="88" height="31" vspace="8" border="0" /></a>
+                <a href="http://jigsaw.w3.org/css-validator/check/referer"><img style="border:0;width:88px;height:31px"  src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Valid CSS!" vspace="8" border="0" /></a>
+            </div>
+            
+            <div class="cleaner"></div>
+            
+            <div id="templatemo_footer">
+                Copyright 漏 2024 <a href="#">Your Company Name</a> | from <a href="http://www.cssmoban.com/" target="_parent">缃戠珯妯℃澘</a>        
+            </div>  
+        </div>
+    </div>
+<!--  缃戠珯妯℃澘 from cssMoban.com  -->
+</body>
 </html>
